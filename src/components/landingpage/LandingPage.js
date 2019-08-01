e407fc4612e2ad8b30dd8aeffea67f5d14b6e316
jasoncarcamo <jasoncarcamo30@yahoo.com>
2019-07-31 20:27:25 -0400

    update

--- src/components/landingpage/LandingPage.js
+++ src/components/landingpage/LandingPage.js
@@ -25,7 +25,7 @@ class LandingPage extends React.Component{
 
                     <section id="child_friendly">   
 
-                        <img src={cleanCrib}/>
+                        <img src={cleanCrib} alt="A clean baby crib."/>
                         <h3>Green Friendly</h3>
                         <p>We use environmentally-conscious products that are safe for people and pets.</p>
 
@@ -33,7 +33,7 @@ class LandingPage extends React.Component{
 
                     <section id="clean_kitchen">
 
-                        <img src={cleanKitchen}/>
+                        <img src={cleanKitchen} alt="A clean kitchen"/>
                         <h3>Residential and Commercial cleaning</h3>
                         <p>From deep cleaning your kitchen and bathrooms to changing your bed linens, we are dedicated to creating clean, safe and healthy environments for both commercial and residential spaces.</p>
 
@@ -41,7 +41,7 @@ class LandingPage extends React.Component{
 
                     <section id="clean_office">
 
-                        <img src={cleanOffice}/>
+                        <img src={cleanOffice} alt="A clean office"/>
                         <h3>Customer satisfaction</h3>
                         <p>Whether you need weekly, every other week, monthly, or occasional housekeeping services, our professionals will leave you feeling comfortable in and proud of your space.</p>
 
@@ -50,7 +50,7 @@ class LandingPage extends React.Component{
                 </section>
 
                 <section id="living_room_section">
-                    <img id="clean_living_room" src={cleanLivingRoom}/>
+                    <img id="clean_living_room" src={cleanLivingRoom} alt="A clean living room."/>
                     <p>Coming home after a long day to a place that’s clean, orderly, and welcoming does not have to be a fantasy. That’s why we take the time to offer services that work with your budget, cleaning preferences, and busy lifestyle.  Our friendly professionals strive to earn your loyalty every time we clean, which is why we make setting up house cleaning services as easy as can be. Whether you are looking to request services one time or create a routine cleaning plan, you'll never have to worry about signing a contract.</p>
 
                 </section>
